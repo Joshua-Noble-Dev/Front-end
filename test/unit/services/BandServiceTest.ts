695289fbd9f8f30fec1e6aaf56c7f438c0a03ce3
Anne Moore <anne.moore@kainos.com>
2024-07-31 08:51:53 +0100

    Adding tests

--- test/unit/services/BandServiceTest.ts
+++ test/unit/services/BandServiceTest.ts
@@ -1,9 +1,47 @@
 import axios from "axios";
+import { Band } from "../../../src/models/Band";
+import { getBands } from "../../../src/services/BandService";
 import MockAdapter from "axios-mock-adapter";
+import { expect } from "chai";
 
+const bands: Band = {
+    bandId: 1,
+    bandName: "Entry"
+}
 
 const mock = new MockAdapter(axios);
 
-describe('CapabilityService', function () {
+describe('BandService', function () {
 
-})
+    describe('getBands', function () {
+        it('should return bands from response', async () => {
+
+          const data = [bands];
+  
+          mock.onGet("http://localhost:8080/api/bands").reply(200, data);
+  
+          const results = await getBands();
+
+          console.log(results[0].bandId);
+          console.log(results[0].bandName);
+  
+          expect(results[0].bandId).to.deep.equal(bands.bandId);
+          expect(results[0].bandName).to.deep.equal(bands.bandName);
+        })
+  
+        it('should return Failed to get Bands when error returned from axios', async () => {
+    
+            mock.onGet("http://localhost:8080/api/bands").reply(500);
+
+            try {
+                await getBands();
+                throw new Error('Error not thrown');
+              } catch (e) {
+                expect(e.message).to.equal('Failed to get bands');
+                return;
+              }
+        })
+
+      })
+
+});
--- test/unit/services/CapabilityServiceTest.ts
+++ test/unit/services/CapabilityServiceTest.ts
@@ -1,9 +1,46 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { Capability } from "../../../src/models/Capability";
+import { getCapabilities } from "../../../src/services/CapabilityService";
+import { expect } from "chai";
 
+const capabilities: Capability = {
+    capabilityId: 1,
+    capabilityName: "Engineering"
+} 
 
 const mock = new MockAdapter(axios);
 
-describe('BandService', function () {
+describe('CapabilityService', function () {
 
+    describe('getCapabilities', function () {
+        it('should return bands from response', async () => {
+
+          const data = [capabilities];
+  
+          mock.onGet("http://localhost:8080/api/capabilities").reply(200, data);
+  
+          const results = await getCapabilities();
+
+          console.log(results[0].capabilityId);
+          console.log(results[0].capabilityName);
+  
+          expect(results[0].capabilityId).to.deep.equal(capabilities.capabilityId);
+          expect(results[0].capabilityName).to.deep.equal(capabilities.capabilityName);
+        })
+  
+        it('should return Failed to get Bands when error returned from axios', async () => {
+    
+            mock.onGet("http://localhost:8080/api/capabilities").reply(500);
+
+            try {
+                await getCapabilities();
+                throw new Error('Error not thrown');
+              } catch (e) {
+                expect(e.message).to.equal('Failed to get capabilities');
+                return;
+              }
+        })
+
+      })
 });
